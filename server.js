1d7c341309054ec3fda83e35205241cc79950f62
Jean Freitas <jeanphfreitas@gmail.com>
2022-02-01 20:48:41 -0800

    commit test

--- server.js
+++ server.js
@@ -9,4 +9,6 @@ app.get("/", (req, res) => res.send("API Runing"));
 
 const PORT = process.env.POR || 5000;
 
+// igor cuzao
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
