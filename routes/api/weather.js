72c266b080897f205a1495737f918eb767436438
Igor <ipaiva07@hotmail.com>
2022-02-03 21:25:45 -0800

    City route created

--- routes/api/weather.js
+++ routes/api/weather.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+const axios = require("axios");
+
 // @route    GET api/weather
 // @desc     Test route
 // @access   Public
 router.get("/", (req, res) => res.send("Weather route"));
+
+// @route    GET api/weather/city
+// @desc     Get City by name
+// @access   Public
+router.get("/city/:cityname", async (req, res) => {
+  try {
+    const uri = encodeURI(
+      `https://openweathermap.org/data/2.5/find?q=${req.params.cityname}&type=like&sort=population&cnt=30&appid=439d4b804bc8187953eb36d2a8c26a02`
+    );
+
+    const cityResponse = await axios.get(uri);
+    return res.json(cityResponse.data);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(404).json({ msg: "Server Error" });
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.get("/", (req, res) => res.send("API Runing"));
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/city", require("./routes/api/city"));
 app.use("/api/weather", require("./routes/api/weather"));
 
 const PORT = process.env.POR || 5000;
