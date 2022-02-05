20c29a3d9a306af634cea6e59b12a9a3dca6e2c8
Igor <ipaiva07@hotmail.com>
2022-02-05 11:56:50 -0800

    Weather coordenates route

--- routes/api/weather.js
+++ routes/api/weather.js
@@ -1,14 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
-
-// TODO - is it the best solution?
-const cityList = [];
-
-// @route    GET api/weather
-// @desc     Test route
-// @access   Public
-router.get("/", (req, res) => res.send("Weather route"));
+const config = require("config");
 
 // @route    GET api/weather/city
 // @desc     Get City by name
@@ -16,13 +9,15 @@ router.get("/", (req, res) => res.send("Weather route"));
 router.get("/city/:cityname", async (req, res) => {
   try {
     const uri = encodeURI(
-      `https://openweathermap.org/data/2.5/find?q=${req.params.cityname}&type=like&sort=population&cnt=30&appid=439d4b804bc8187953eb36d2a8c26a02`
+      `https://openweathermap.org/data/2.5/find?q=${
+        req.params.cityname
+      }&type=like&sort=population&cnt=30&appid=${config.get("appid")}`
     );
-
+    const cityList = [];
     const cityResponse = await axios.get(uri);
     const data = cityResponse.data;
-    
-    data.list.map(cityObj => {
+
+    data.list.map((cityObj) => {
       let city = new Object();
 
       city.name = cityObj.name;
@@ -30,9 +25,40 @@ router.get("/city/:cityname", async (req, res) => {
       city.country = cityObj.sys.country;
 
       cityList.push(city);
-    })
+    });
 
     return res.json(cityList);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(404).json({ msg: "Server Error" });
+  }
+});
+
+// @route    GET api/weather
+// @desc     Test route
+// @access   Public
+router.get("/:lat/:lon", async (req, res) => {
+  try {
+    const uri = encodeURI(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${
+        req.params.lat
+      }&lon=${req.params.lon}&appid=${config.get("apikey")}&units=metric`
+    );
+
+    const weatherResponse = await axios.get(uri);
+    const data = weatherResponse.data;
+
+    // data.list.map((cityObj) => {
+    //   let city = new Object();
+
+    //   city.name = cityObj.name;
+    //   city.coord = cityObj.coord;
+    //   city.country = cityObj.sys.country;
+
+    //   cityList.push(city);
+    // });
+
+    return res.json(data);
   } catch (err) {
     console.error(err.message);
     return res.status(404).json({ msg: "Server Error" });
